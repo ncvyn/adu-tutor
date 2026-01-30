f2140065311963d8a0fe63ac51927c873c05d77c
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-01-30 16:24:49 +0800

    Create landing page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { Dock, Navbar } from '@/components'
+
+export const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Dock />
+    </>
+  )
+}
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,33 @@
+import { createSignal } from 'solid-js'
+import AdULogo from '@/adulogo.png'
+
+export const LandingPage = () => {
+  const [isLoading, setIsLoading] = createSignal(false)
+  const logIn = async () => {
+    setIsLoading(true)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    setIsLoading(false)
+  }
+
+  return (
+    <div class="hero bg-base-200 min-h-screen">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Welcome to AdU-Tutor</h1>
+          <p class="py-6">
+            AdU-Tutor is a platform that connects students with tutors for
+            online tutoring services.
+          </p>
+          <button
+            onClick={logIn}
+            classList={{ loading: isLoading() }}
+            class="btn btn-primary"
+          >
+            <img class="w-6" src={AdULogo} alt="Adamson University logo" />
+            Login with AdU Mail
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
