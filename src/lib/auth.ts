aace3b77efa85f5500b3666bf1a13bd9907d0b3e
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-01-31 20:09:15 +0800

    Deploy temp wrapper for debugging

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,11 +3,15 @@ import { tanstackStartCookies } from 'better-auth/tanstack-start/solid'
 import { env } from 'cloudflare:workers'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { user, session, account, verification } from '@/../auth-schema'
+import { drizzle } from 'drizzle-orm/d1'
+
+const db = drizzle(env.adu_tutor_d1)
+const schema = { user, session, account, verification }
 
 export const auth = betterAuth({
-  database: drizzleAdapter(env.adu_tutor_d1, {
+  database: drizzleAdapter(db, {
     provider: 'sqlite',
-    schema: { user, session, account, verification },
+    schema: schema,
   }),
   socialProviders: {
     microsoft: {
