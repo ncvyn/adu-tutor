d7c4e5484a73ea706e113343eb9debcc7184915e
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-03-16 17:27:05 +0800

    Update packages
    Notable changes are:
    vite ^7.1.7/^7.3.1 -> ^8.0.0
    better-auth ^1.4.18 -> ^1.5.5

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
@@ -10,14 +9,13 @@ import { cloudflare } from '@cloudflare/vite-plugin'
 import lucidePreprocess from 'vite-plugin-lucide-preprocess'
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     lucidePreprocess(),
     devtools(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
     tailwindcss(),
     tanstackStart(),
     solidPlugin({ ssr: true }),
