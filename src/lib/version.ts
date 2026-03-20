58c1a4e25c2e96b96b48c7a2a5b1f7a18ac85c77
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-03-21 00:14:31 +0800

    Bump version.ts before I forget

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -4,4 +4,4 @@
 // - Increment the PATCH version to fix errors if the previous build failed.
 
 // This *could* be automated but I do not want to spend 10 hours to shave off 1 second yet again...
-export const APP_VERSION = '0.65.0'
+export const APP_VERSION = '0.66.0'
