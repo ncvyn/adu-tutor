c02df7a67bdc1ce4a021a41f7c17c55120529e7e
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-03-24 02:17:50 +0800

    Update version.ts
    The final research defense was cancelled a few days ago... (rip)

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,7 +1,7 @@
 // Versioning guidelines:
-// - Increment the MAJOR version after the research defense is finished.
+// - Increment the MAJOR version to 1 after the web app is feature complete.
 // - Increment the MINOR version before uploading new code to Cloudflare Workers.
 // - Increment the PATCH version to fix errors if the previous build failed.
 
 // This *could* be automated but I do not want to spend 10 hours to shave off 1 second yet again...
-export const APP_VERSION = '0.68.0'
+export const APP_VERSION = '0.69.0'
