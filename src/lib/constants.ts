dcd4bf42e55fb8d822709f5ee944524d2e31ef19
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-03-26 20:30:45 +0800

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,18 +1,15 @@
 export const SUBJECTS = [
   'Math',
-
   'Programming',
-
+  'Engineering',
   'Chemistry',
   'Biology',
   'Physics',
-
   'English',
   'PerDev',
-
   'Research',
-
   'General',
+  'Others',
 ] as const
 
 export const DAYS = [
